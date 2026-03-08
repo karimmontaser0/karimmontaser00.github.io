16814607dabad8a8d151af613a1ae7ff4e4e3804
karimmontaser0 <karimmontaser40@gmail.com>
2026-03-08 02:19:48 +0200

    Fix base URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,8 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  site: "https://karimmontaser00.github.io",
+  site: "https://karimmontaser0.github.io",
+  base: "karimmontaser00.github.io",
   vite: {
     plugins: [tailwindcss()],
   },
